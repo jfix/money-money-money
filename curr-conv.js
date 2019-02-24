f157c8d917f66f381e0c7fed7b068d268be8397a
Jakob Fix <jakob.fix@gmail.com>
2019-02-24 15:06:26 +0100

    Mock currency converter using fixer.io object structure

---
+++ curr-conv.js
@@ -0,0 +1,18 @@
+
+const mockConverter = () => (
+    {
+        "success": true,
+        "timestamp": 1551002345,
+        "base": "EUR",
+        "date": "2019-02-24",
+        "rates":
+        {
+            "GBP":0.868853,
+            "USD":1.134153,
+            "JPY":125.556489,
+            "MXN":21.71371
+       }
+    }
+)
+
+module.exports = mockConverter
